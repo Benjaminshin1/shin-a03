c67ea161bf0cd00dc5bd1022cf2c754fe59e3940
Benjaminshin <Benjaminshin92@gmail.com>
2021-09-26 18:47:47 -0400

    complete

--- assignment30/src/main/java/baseline/solution30.java
+++ assignment30/src/main/java/baseline/solution30.java
@@ -2,12 +2,7 @@
  *  UCF COP3330 Fall 2021 Assignment 30 Solution
  *  Copyright 2021 Benjamin Shin
  */
-/*
-Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
-constraints
-Use a nested loop to complete this program.
-Align each column within the table without using the tab character.
- */
+
 package baseline;
 
 import java.util.Scanner;
@@ -15,12 +10,22 @@ import java.util.Scanner;
 public class solution30 {
     public static final Scanner input = new Scanner(System.in);
     
-    public void create_table(){
+    public void create_table() {
         //create a 12 by 12 table
-        
+        int i;
+        int j;
+        //Use a nested loop to complete this program.
+        for (i = 1; i <= 12; ++i) {
+            for (j = 1; j <= 12; ++j) {
+                //multiply and print the numbers using %4d to tidy up the format
+                System.out.printf("%4d", (i*j));
+            }
+            System.out.println();
+        }
     }
-
     public static void main(String[] args) {
-
+        solution30 app = new solution30();
+        //call the multiplication table
+        app.create_table();
     }
 }
