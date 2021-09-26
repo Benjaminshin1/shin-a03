a83e2aa550a27c330eba7a3d7521834786072515
Benjaminshin <Benjaminshin92@gmail.com>
2021-09-26 15:50:29 -0400

    complete

--- assignment28/src/main/java/baseline/solution28.java
+++ assignment28/src/main/java/baseline/solution28.java
@@ -5,15 +5,7 @@ package baseline;
  */
 //Write a program that prompts the user for five numbers and computes the total of the numbers.
 //The prompting must use repetition, such as a counted loop, not three separate prompts.
-/*
-example
-Enter a number: 1
-Enter a number: 2
-Enter a number: 3
-Enter a number: 4
-Enter a number: 5
-The total is 15.
- */
+
 
 import java.util.Scanner;
 
@@ -21,12 +13,16 @@ public class solution28 {
     public static final Scanner input = new Scanner(System.in);
 
     public void sum(){
+        solution28 app = new solution28();
         int i=0;
+        int sum=0;
         //using a counted loop
         for(i=0;i<5;i++){
             //repeat the loop 5 times asking for an input
-            
+            int number = app.input_int("Enter a number: ");
+            sum = number + sum;
         }
+        System.out.println(sum);
     }
 
     public int input_int(String prompt){
@@ -36,6 +32,7 @@ public class solution28 {
     }
 
     public static void main(String[] args) {
-
+        solution28 app = new solution28();
+        app.sum();
     }
 }
