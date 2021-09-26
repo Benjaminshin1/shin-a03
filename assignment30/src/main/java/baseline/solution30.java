aa2872bf2e9397c1137818cf59cf79625089f9ce
Benjaminshin <Benjaminshin92@gmail.com>
2021-09-26 18:41:03 -0400

    pseudocode

---
+++ assignment30/src/main/java/baseline/solution30.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 30 Solution
+ *  Copyright 2021 Benjamin Shin
+ */
+/*
+Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
+constraints
+Use a nested loop to complete this program.
+Align each column within the table without using the tab character.
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class solution30 {
+    public static final Scanner input = new Scanner(System.in);
+    
+    public void create_table(){
+        //create a 12 by 12 table
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
