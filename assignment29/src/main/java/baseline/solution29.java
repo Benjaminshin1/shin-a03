7f5634d7cc4de79fdda52b9c875690edfde2956c
Benjaminshin <Benjaminshin92@gmail.com>
2021-09-26 18:09:58 -0400

    complete

--- assignment29/src/main/java/baseline/solution29.java
+++ assignment29/src/main/java/baseline/solution29.java
@@ -5,32 +5,62 @@ package baseline;
  */
 /*
 Write a quick calculator that prompts for the rate of return on an investment and calculates how many years it will take to double your investment.
-
-example
-What is the rate of return? 0
-Sorry. That's not a valid input.
-What is the rate of return? ABC
-Sorry. That's not a valid input.
-What is the rate of return? 4
-It will take 18 years to double your initial investment.
  */
 
 import java.util.Scanner;
 
 public class solution29 {
     public static final Scanner input = new Scanner(System.in);
-    public void calculate(){
-        //years = 72 / r
+
+    public int calculate(int r) {
+        int years = 72 / r;
         //where r is the stated rate of return.
+        return years;
     }
 
-    public int input_int(String prompt){
+    public String input_string(String prompt) {
         //create a scanner function for the rate of return
         System.out.println(prompt);
-        return input.nextInt();
+        return input.next();
     }
 
-    public static void main(String[] args) {
+    public int loop() {
+        int r = 0;
+        solution29 app = new solution29();
+        boolean status = true;
 
+        //repeats if status is true
+        while (status) {
+            String n = app.input_string("");
+
+            for (int i = 0; i < n.length(); i++) {
+
+                //If we find a non-digit character we return true.
+                if (!Character.isDigit(n.charAt(i))) {
+                    status = true;
+                } else
+                    //change value to int
+                    r = Integer.parseInt(n);
+            }
+
+                if (r<1) {
+                    //checks value if greater than 0
+                    //if it is break loop
+                    System.out.println("enter a valid number.");
+                    status = true;
+                } else break;
+            }
+        return r;
+        }
+
+        public static void main (String[]args){
+            solution29 app = new solution29();
+
+            System.out.println("What is the rate of return?");
+            int r = app.loop();
+            int month= app.calculate(r);
+            System.out.println("It will take "+month+" years to double your initial investment.");
+
+        }
     }
-}
+
