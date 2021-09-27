7af371c17ab6080ab6c5f8be2ef5f1a41d2a95f6
Benjaminshin <Benjaminshin92@gmail.com>
2021-09-27 00:19:39 -0400

    psuedocode

---
+++ excercise31/src/main/java/baseline/solution31.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 31 Solution
+ *  Copyright 2021 Benjamin Shin
+ */
+/*
+Use the Karvonen formula to determine the target heart rate based on a range of intensities from 55% to 95%. Generate a table with the results as shown in the example output.
+The formula is TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class solution31 {
+    public static final Scanner input = new Scanner(System.in);
+
+    public void loop(int age, int restingHR){
+        //using a loop generate a table using the results from the formula
+    }
+
+    public static void main(String[] args) {
+        solution31 app = new solution31();
+        int age;
+
+        int restingHR;
+        //dont continue until a proper input is given
+
+        //print format
+        System.out.println();
+    }
+}
