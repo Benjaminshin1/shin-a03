18528d2927e367978abc3734385c2002fcc9c501
Benjaminshin <Benjaminshin92@gmail.com>
2021-09-29 19:35:56 -0400

    pseudocode

---
+++ excercise33/src/main/java/baseline/solution33.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 33 Solution
+ *  Copyright 2021 Benjamin Shin
+ */
+/*
+Create a Magic 8 Ball game that prompts for a question and then displays either "Yes," "No," "Maybe," or "Ask again later."
+The value should be chosen randomly using a pseudo random number generator. Store the possible choices in a list and select one at random.
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class solution33 {
+    public static final Scanner input = new Scanner(System.in);
+
+    public int randomnumber(){
+        //return a random number 1-4 to generate a random question
+        int number=0;
+        return number;
+    }
+
+    public void give_wisdom(int number){
+        //give an answer based on the number generated
+    }
+
+    public static void main(String[] args) {
+        //prompt user to ask for a question
+        //answer user using a randomly generated number generator to give a random answer
+
+    }
+}
