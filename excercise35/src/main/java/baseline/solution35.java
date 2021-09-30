94782e4e4b1a32a0012f31e951183e8d34fc5294
Benjaminshin <Benjaminshin92@gmail.com>
2021-09-29 21:22:28 -0400

    psuedocode

---
+++ excercise35/src/main/java/baseline/solution35.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 35 Solution
+ *  Copyright 2021 Benjamin Shin
+ */
+/*
+Create a program that picks a winner for a contest or prize drawing.
+Prompt for names of contestants until the user leaves the entry blank. Then randomly select a winner.
+
+Some languages require that you define the length of the array ahead of time. You may need to find another data structure, like an ArrayList.
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class solution35 {
+    public static final Scanner input = new Scanner(System.in);
+
+    public void winner_list(){
+        //Use a loop to capture user input into an array.
+        //Don’t include a blank entry in the array.
+
+        //generate a number based on how many people there are
+
+        //output the winner based on the number
+    }
+
+    public void RNG(){
+        //Use a random number generator to pluck a value from the array.
+    }
+
+    public static void main(String[] args) {
+//call winner_list function to get the array list
+    }
+}
