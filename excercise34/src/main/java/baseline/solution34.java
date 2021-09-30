f0d879e70f96a754bff29bd76bab680ee5d8fd56
Benjaminshin <Benjaminshin92@gmail.com>
2021-09-29 20:20:08 -0400

    psuedocode

---
+++ excercise34/src/main/java/baseline/solution34.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 34 Solution
+ *  Copyright 2021 Benjamin Shin
+ */
+
+/*
+Create a small program that contains a list of employee names.
+ */
+package baseline;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class solution34 {
+    public static final Scanner input = new Scanner(System.in);
+
+    public String get_name() {
+        //save the full name to compare it to an array of names
+        //remove a name by comparing it to the users name that was inputted
+        String name = null;
+        return name;
+    }
+
+public void names(){
+        //hold the list of names
+    List<String> names = new ArrayList<>();
+}
+
+
+    public static void main(String[] args) {
+        solution34 app = new solution34();
+        //Print out the list of names when the program runs the first time.
+        //Prompt for an employee name and remove that specific name from the list of names.
+        //Display the remaining employees, each on its own line.
+    }
+}
