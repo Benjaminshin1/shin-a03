e357ff1c11e70dfc9defed12214b525336c0087d
Benjaminshin <Benjaminshin92@gmail.com>
2021-10-03 18:32:41 -0400

    complete

--- excercise39/src/main/java/baseline/solution39.java
+++ excercise39/src/main/java/baseline/solution39.java
@@ -2,31 +2,19 @@
  *  UCF COP3330 Fall 2021 Assignment 39 Solutions
  *  Copyright 2021 Benjamin Shin
  */
-/*
-Given the following data set, create a program that sorts all employees by last name and prints them to the screen in a tabular format.
-
-example output
-Name                | Position          | Separation Date
---------------------|-------------------|----------------
-Jacquelyn Jackson   | DBA               |
-Jake Jacobson       | Programmer        |
-John Johnson        | Manager           | 2016-12-31
-Michaela Michaelson | District Manager  | 2015-12-19
-Sally Weber         | Web Developer     | 2015-12-18
-Tou Xiong           | Software Engineer | 2016-10-05
-
-Store the data using a list of maps.
- */
 package baseline;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+//constructor
 class Employee {
-    private String first;
-    private String last;
-    private String position;
-    private String separationDate;
+    public String first;
+    public String last;
+    public String position;
+    public String separationDate;
 
     public Employee(String first, String last, String position, String separationDate) {
         this.first = first;
@@ -34,17 +22,56 @@ class Employee {
         this.position = position;
         this.separationDate = separationDate;
     }
-
+    public String getFirst() {
+        return first;
+    }
+    public String getLast() {
+        return last;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public String getSeparationDate() {
+        return separationDate;
+    }
+}
+//A comparison function, which imposes a total ordering on some collection of objects.
+// Comparators can be passed to a sort method (such as Collections.sort or Arrays.sort) to allow precise control over the sort order.
+// Comparators can also be used to control the order of certain data structures (such as sorted sets or sorted maps),
+// or to provide an ordering for collections of objects that don't have a natural ordering.
+class sortlastname implements Comparator<Employee> {
+    @Override
+    public int compare(Employee one, Employee two) {
+        return one.getLast().compareTo(two.getLast());
+    }
 }
 
-public class solution39 {
 
-    public void compare(){
-        //compare employee last name to sort
-    }
+public class solution39 {
 
     public static void main(String[] args) {
         //array list of the entire employee record
         List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("John", "Johnson", "Manager", "2016-12-31"));
+        employeeList.add(new Employee("Tou", "Xiong", "Software Engineer", "2016-10-05"));
+        employeeList.add(new Employee("Michaela", "Michaelson", "District Manager", "2015-12-19"));
+        employeeList.add(new Employee("Jake", "Jacobson", "Programmer", ""));
+        employeeList.add(new Employee("Jacquelyn", "Jackson", "DBA", ""));
+        employeeList.add(new Employee("Sally", "Weber", "Web Developer", "2015-12-18"));
+
+        Collections.sort(employeeList, new sortlastname());
+
+        //format of name and position and separation date
+        System.out.println("Name" + "                 |  " + "Position            | "+"separation date");
+        System.out.println("---------------------|----------------------|----------------------");
+
+        //uses a loop to print employee data
+        for (Employee employee : employeeList) {
+            //%1 2 3 and 4 represent the .get
+            String format = "%1$-10s %2$-10s| %3$-20s | %4$-20s\n";
+            System.out.format(format, employee.getFirst(), employee.getLast(), employee.getPosition(),employee.getSeparationDate());
+
+
+        }
     }
 }
